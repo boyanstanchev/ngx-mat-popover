d985c7fda7d3b0541f7ed4b9a84cc058b3c6ebf4
Boyan Stanchev <boyan_stan4ev@hotmail.com>
2024-02-29 22:46:49 +0200

    added imports

--- projects/ngx-mat-popover/src/lib/ngx-mat-popover.module.ts
+++ projects/ngx-mat-popover/src/lib/ngx-mat-popover.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
-import { NgxMatPopoverComponent } from './ngx-mat-popover.component';
-
-
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxMatPopoverContainerComponent } from './ngx-mat-popover-container.component';
+import { NgxMatPopoverDirective } from './ngx-mat-popover.directive';
 
 @NgModule({
-  declarations: [
-    NgxMatPopoverComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    NgxMatPopoverComponent
-  ]
+	declarations: [NgxMatPopoverContainerComponent, NgxMatPopoverDirective],
+	imports: [OverlayModule, MatCardModule, MatButtonModule, MatIconModule],
+	exports: [NgxMatPopoverDirective],
 })
-export class NgxMatPopoverModule { }
+export class NgxMatPopoverModule {}
