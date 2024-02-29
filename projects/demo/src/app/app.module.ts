8724dc6e2362ca258990b532015b1090085d3200
Boyan Stanchev <boyan_stan4ev@hotmail.com>
2024-02-29 22:42:15 +0200

    removed routing from demo

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgxMatPopoverModule } from 'projects/ngx-mat-popover/src/public-api';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, NgxMatPopoverModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
