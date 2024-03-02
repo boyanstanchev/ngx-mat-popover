98da136befc0bdc24f9eaa7d4e2c6189c96c568e
Boyan Stanchev <boyan_stan4ev@hotmail.com>
2024-03-02 18:01:47 +0200

    added required modules and basic demo

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxMatPopoverModule } from 'projects/ngx-mat-popover/src/public-api';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, NgxMatPopoverModule],
+	imports: [
+		CommonModule,
+		BrowserModule,
+		NgxMatPopoverModule,
+		MatButtonModule,
+		MatIconModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
